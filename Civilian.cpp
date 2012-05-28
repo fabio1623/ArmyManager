fb4a46092cd06eebfa7ff11e48b1ecc5b94ff1f8
Adnan RIHAN <axel50397@gmail.com>
2012-05-28 19:20:47 +0200

    Initial commit Civilian

--- Civilian.cpp
+++ Civilian.cpp
@@ -6,4 +6,56 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#include <iostream>
+#include "Civilian.hpp"
+
+Civilian(std::string fname, std::string lname, std::string ctry, int sgh)
+{
+	this->firstname = fname;
+	this->lastname = lname;
+	this->country = ctry;
+	this->strengh = sgh;
+}
+
+~Civilian()
+{
+}
+
+std::string	getFirstname(void) const
+{
+	return (this->firstname);
+}
+
+void		setFirstname(std::string const &fname)
+{
+	this->firstname = fname;
+}
+
+std::string	getLastname(void) const
+{
+	return (this->lastname);
+}
+
+void		setLastname(std::string const &lname)
+{
+	this->lastname = lname;
+}
+
+std::string	getCountry(void) const
+{
+	return (this->country);
+}
+
+void		setCountry(std::string const &ctry)
+{
+	this->country = ctry;
+}
+
+int			getStrengh(void) const
+{
+	return (this->strengh);
+}
+
+void		setStrengh(int const &sgh)
+{
+	this->strengh = sgh;
+}
--- Civilian.hpp
+++ Civilian.hpp
@@ -11,4 +11,25 @@
 
 #include "Global.hpp"
 
+class	Civilian
+{
+	private:
+	std::string	firstname;
+	std::string lastname;
+	std::string	country;
+	int			strengh;
+
+	public:
+	Civilian(std::string fname, std::string lname, std::string ctry, int sgh = 0);
+	~Civilian();
+	std::string	getFirstname(void) const;
+	void		setFirstname(std::string const &fname);
+	std::string	getLastname(void) const;
+	void		setLastname(std::string const &lname);
+	std::string	getCountry(void) const;
+	void		setCountry(std::string const &ctry);
+	int			getStrengh(void) const;
+	void		setStrengh(int const &sgh);
+};
+
 #endif
